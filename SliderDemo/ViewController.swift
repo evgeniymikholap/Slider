ecc17b2b9370020cea3ccc9ef2222bfe8b489c3c
Evgeniy Mikholap <evgeniy@mikholap.com>
2018-06-05 16:03:43 +0300

    Init

---
+++ SliderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderDemo
+//
+//  Created by Evgeniy Mikholap on 6/5/18.
+//  Copyright © 2018 Evgeniy Mikholap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
