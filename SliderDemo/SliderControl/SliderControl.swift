9dc48545cb1640cb6487edfc806fc3dc1c8db0a1
Evgeniy Mikholap <evgeniy@mikholap.com>
2018-06-06 16:59:41 +0300

    Improved UI

--- SliderDemo/SliderControl/SliderControl.swift
+++ SliderDemo/SliderControl/SliderControl.swift
@@ -6,30 +6,125 @@
 //  Copyright © 2018 Evgeniy Mikholap. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 
+/// Slider Control constants
 private struct SliderConstants {
 
-    static let minHeight: CGFloat = 44
-    static let minWidth: CGFloat = 100
-    static let maxHeight: CGFloat = 100
-    static let numberOfLabels = 2
-    static let animationDuration = 0.3
-    static let sliderViewShadowOpacity: Float = 0.2
+    static let minHeight: CGFloat              = 44
+    static let minWidth: CGFloat               = 100
+    static let maxHeight: CGFloat              = 100
+    static let numberOfLabels                  = 2
+    static let defaultSliderViewInset: CGFloat = 4
+    static let defaultSliderLabelFont          = UIFont.systemFont(ofSize: 15, weight: .medium)
+    static let defaultLabelFont                = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static let animationDuration               = 0.3
+    static let sliderViewShadowOpacity: Float  = 0.2
     static let sliderViewShadowRadius: CGFloat = 2
-    static let sliderViewShadowOffset = CGSize(width: 0, height: 0)
+    static let sliderViewShadowOffset          = CGSize(width: 0, height: 0)
 
 }
 
 
-@IBDesignable
-open class SliderControl: UIControl {
+/// Slider Control color
+private struct SliderDefaultColors {
 
-    // MARK: Public variables
+    static let leftSideBackgroundColor     = #colorLiteral(red: 0, green: 0.7307787538, blue: 0.6325929761, alpha: 1)
+    static let rightSideBackgroundColor    = #colorLiteral(red: 0.5369121432, green: 0.5369251966, blue: 0.5369181633, alpha: 1)
+    static let sliderFirstBackgroundColor  = #colorLiteral(red: 0.9921568627, green: 0.9882352941, blue: 0.9803921569, alpha: 1)
+    static let sliderSecondBackgroundColor = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8705882353, alpha: 1)
+    static let sliderTextColor             = #colorLiteral(red: 0.7176470588, green: 0.7137254902, blue: 0.7333333333, alpha: 1)
+    static let leftLabelTextColor          = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let rightLabelTextColor         = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
 
-    var leftSideTitle: String {
+}
+
+
+/// Slider Control states
+public enum SliderState {
+    case normal
+    case error
+    case disabled
+}
+
+
+/// Public interface for the Slider Control
+public protocol SliderControlProtocol {
+    /// Left label title
+    var leftSideTitle: String { get set }
+
+    /// Right label title
+    var rightSideTitle: String { get set }
+
+    /// Slider label title
+    var sliderTitle: String { get set }
+
+    /// Selected Index: 0 — left slider position, 1 — right slider position. Default: 0
+    var selectedIndex: Int { get set }
+
+    /// Slider Control state. Default: .normal
+    var controlState: SliderState { get set }
+
+    /// Slider View inset. Default: SliderConstants.defaultSliderViewInset
+    var sliderLabelInset: CGFloat { get set }
+
+    /// Enable/Disable flag
+    var isEnabled: Bool { get set }
+
+    /// Slider label font
+    var sliderTitleFont: UIFont { get set }
+
+    /// Left label font
+    var leftLabelTitleFont: UIFont { get set }
+
+    /// Right label font
+    var rightLabelTitleFont: UIFont { get set }
+
+    /// Left side view background color. IBInspectable. Default: SliderColors.leftSideBackgroundColor
+    var leftSideBackgroundColor: UIColor { get set }
+
+    /// Right side view background color. IBInspectable. Default: SliderColors.rightSideBackgroundColor
+    var rightSideBackgroundColor: UIColor { get set }
+
+    /// Top color of Slider view gradient. IBInspectable. Default: SliderColors.sliderFirstBackgroundColor
+    var sliderFirstBackgroundColor: UIColor { get set }
+
+    /// Bottom color of Slider view gradient. IBInspectable. Default: SliderColors.sliderSecondBackgroundColor
+    var sliderSecondBackgroundColor: UIColor { get set }
+
+    /// Slider label text color. Default: SliderColors.sliderTextColor
+    var sliderTextColor: UIColor { get set }
+
+    /// Left label text color. Default: SliderColors.leftLabelTextColor
+    var leftLabelTextColor: UIColor { get set }
+
+    /// Right label text color. Default: SliderColors.rightLabelTextColor
+    var rightLabelTextColor: UIColor { get set }
+
+    /// Shows system activity indicator
+    func showActivityIndicator()
+
+    /// Hides system activity indicator
+    func hideActivityIndicator()
+
+    /**
+        Initializes a new Slider Control
+
+        - Parameter sliderTitle: slider label title
+
+        - Returns: A new Slider Control
+     */
+    init(sliderTitle: String, leftSideTitle: String, rightSideTitle: String)
+}
+
+
+/// Slider Control class
+@IBDesignable public class SliderControl: UIControl, SliderControlProtocol {
+
+    // MARK: Public interface. SliderControlProtocol
+
+    public var leftSideTitle: String {
         get {
             return leftLabel.text!
         }
@@ -38,10 +133,12 @@ open class SliderControl: UIControl {
             leftLabel.textAlignment = .left
             leftLabel.font = leftLabelTitleFont
             leftLabel.textColor = leftLabelTextColor
+            leftLabel.minimumScaleFactor = 0.5
+            leftLabel.adjustsFontSizeToFitWidth = true
             leftLabel.numberOfLines = 0
         }
     }
-    var rightSideTitle: String {
+    public var rightSideTitle: String {
         get {
             return rightLabel.text!
         }
@@ -50,10 +147,12 @@ open class SliderControl: UIControl {
             rightLabel.textAlignment = .right
             rightLabel.font = rightLabelTitleFont
             rightLabel.textColor = rightLabelTextColor
+            rightLabel.minimumScaleFactor = 0.5
+            rightLabel.adjustsFontSizeToFitWidth = true
             rightLabel.numberOfLines = 0
         }
     }
-    var sliderTitle: String {
+    public var sliderTitle: String {
         get {
             return sliderViewLabel.text!
         }
@@ -62,89 +161,87 @@ open class SliderControl: UIControl {
             sliderViewLabel.textAlignment = .center
             sliderViewLabel.font = sliderTitleFont
             sliderViewLabel.textColor = sliderTextColor
+            sliderViewLabel.minimumScaleFactor = 0.5
+            sliderViewLabel.adjustsFontSizeToFitWidth = true
             sliderViewLabel.numberOfLines = 0
         }
     }
-
-    var isOn = true
-    fileprivate(set) open var selectedIndex = 0
-
-    var sliderLabelInset: CGFloat = 4 {
+    public var selectedIndex = 0
+    public var controlState: SliderState = .normal
+    public var sliderLabelInset: CGFloat = SliderConstants.defaultSliderViewInset {
         didSet {
             setNeedsLayout()
             setNeedsDisplay()
         }
     }
-
-    var sliderTitleFont = UIFont.systemFont(ofSize: 15, weight: .medium) {
+    public override var isEnabled: Bool {
+        didSet {
+            if !isEnabled {
+                controlState = .disabled
+                alpha = alpha / 2
+            } else {
+                controlState = .normal
+                alpha = 1
+            }
+        }
+    }
+    public var sliderTitleFont = SliderConstants.defaultSliderLabelFont {
         didSet {
             sliderViewLabel.font = sliderTitleFont
         }
     }
-    var leftLabelTitleFont = UIFont.systemFont(ofSize: 16, weight: .medium) {
+    public var leftLabelTitleFont = SliderConstants.defaultLabelFont {
         didSet {
             leftLabel.font = leftLabelTitleFont
         }
     }
-    var rightLabelTitleFont = UIFont.systemFont(ofSize: 16, weight: .medium) {
+    public var rightLabelTitleFont = SliderConstants.defaultLabelFont {
         didSet {
             rightLabel.font = rightLabelTitleFont
         }
     }
-
-    @IBInspectable
-    var leftSideBackgroundColor: UIColor = #colorLiteral(red: 0, green: 0.7307787538, blue: 0.6325929761, alpha: 1) {
+    @IBInspectable public var leftSideBackgroundColor: UIColor = SliderDefaultColors.leftSideBackgroundColor {
         didSet {
             leftBackgroundView.backgroundColor = leftSideBackgroundColor
         }
     }
-    @IBInspectable
-    var rightSideBackgroundColor: UIColor = #colorLiteral(red: 0.5369121432, green: 0.5369251966, blue: 0.5369181633, alpha: 1) {
+    @IBInspectable public var rightSideBackgroundColor: UIColor = SliderDefaultColors.rightSideBackgroundColor {
         didSet {
             rightBackgroundView.backgroundColor = rightSideBackgroundColor
         }
     }
-    @IBInspectable
-    var sliderFirstBackgroundColor: UIColor = #colorLiteral(red: 0.9921568627, green: 0.9882352941, blue: 0.9803921569, alpha: 1) {
+    @IBInspectable public var sliderFirstBackgroundColor: UIColor = SliderDefaultColors.sliderFirstBackgroundColor {
         didSet {
             setNeedsDisplay()
         }
     }
-
-    @IBInspectable
-    var sliderSecondBackgroundColor: UIColor = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8705882353, alpha: 1) {
+    @IBInspectable public var sliderSecondBackgroundColor: UIColor = SliderDefaultColors.sliderSecondBackgroundColor {
         didSet {
             setNeedsDisplay()
         }
     }
-
-    @IBInspectable
-    var sliderTextColor: UIColor = #colorLiteral(red: 0.7176470588, green: 0.7137254902, blue: 0.7333333333, alpha: 1) {
+    @IBInspectable public var sliderTextColor: UIColor = SliderDefaultColors.sliderTextColor {
         didSet {
             sliderViewLabel.textColor = sliderTextColor
         }
     }
-
-    @IBInspectable
-    var leftLabelTextColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) {
+    @IBInspectable public var leftLabelTextColor: UIColor = SliderDefaultColors.leftLabelTextColor {
         didSet {
             leftLabel.textColor = leftLabelTextColor
         }
     }
-
-    @IBInspectable
-    var rightLabelTextColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1) {
+    @IBInspectable public var rightLabelTextColor: UIColor = SliderDefaultColors.rightLabelTextColor {
         didSet {
             rightLabel.textColor = rightLabelTextColor
         }
     }
 
-    func showActivityIndicator() {
+    public func showActivityIndicator() {
         activityIndicator.startAnimating()
         sliderViewLabel.isHidden = true
     }
 
-    func hideActivityIndicator() {
+    public func hideActivityIndicator() {
         activityIndicator.stopAnimating()
         sliderViewLabel.isHidden = false
     }
@@ -161,11 +258,14 @@ open class SliderControl: UIControl {
     fileprivate var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
     fileprivate var tapGesture: UITapGestureRecognizer!
     fileprivate var panGesture: UIPanGestureRecognizer!
+    fileprivate let sliderViewShadowLayer = CAShapeLayer()
+    fileprivate let sliderViewGradientLayer = CAGradientLayer()
+    fileprivate var initialSliderLabelFrame: CGRect?
 
 
     // MARK: Initializers
 
-    public init(sliderTitle: String, leftSideTitle: String, rightSideTitle: String) {
+    required public init(sliderTitle: String, leftSideTitle: String, rightSideTitle: String) {
         super.init(frame: CGRect.zero)
 
         self.sliderTitle = sliderTitle
@@ -203,9 +303,6 @@ open class SliderControl: UIControl {
         panGesture.delegate = self
         addGestureRecognizer(panGesture)
     }
-
-    fileprivate let sliderViewShadowLayer = CAShapeLayer()
-    fileprivate let sliderViewGradientLayer = CAGradientLayer()
 
     open override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -245,7 +342,6 @@ open class SliderControl: UIControl {
         setSelectedIndex(index, animated: true)
     }
 
-    var initialSliderLabelFrame: CGRect?
     @objc func pan(_ gesture: UIPanGestureRecognizer!) {
         switch gesture.state {
         case .began:
@@ -269,22 +365,23 @@ open class SliderControl: UIControl {
         }
     }
 
-    open func setSelectedIndex(_ selectedIndex: Int, animated: Bool) {
-        // Reset switch on half pan gestures
-        var catchHalfSwitch = false
-        if self.selectedIndex == selectedIndex {
-            catchHalfSwitch = true
-        }
+    open func setSelectedIndex(_ selectedIndex: Int, animated: Bool, animationDelay: TimeInterval = 0) {
+        let catchHalfSwitch = self.selectedIndex == selectedIndex
 
         self.selectedIndex = selectedIndex
+
         if animated {
-            if (!catchHalfSwitch) {
+            if !catchHalfSwitch && controlState != .error {
                 self.sendActions(for: .valueChanged)
             }
 
-            UIViewPropertyAnimator.runningPropertyAnimator(withDuration: SliderConstants.animationDuration, delay: 0, options: [.beginFromCurrentState, .curveEaseOut], animations: {
+            UIViewPropertyAnimator.runningPropertyAnimator(withDuration: SliderConstants.animationDuration, delay: animationDelay, options: [.beginFromCurrentState, .curveEaseOut], animations: {
                 self.layoutSubviews()
-            }, completion: nil)
+            }, completion: { _ in
+                if self.controlState == .error {
+                    self.sendActions(for: .valueChanged)
+                }
+            })
         } else {
             layoutSubviews()
             sendActions(for: .valueChanged)
@@ -296,6 +393,7 @@ open class SliderControl: UIControl {
 
     override open func layoutSubviews() {
         super.layoutSubviews()
+         setNeedsDisplay()
 
         let sliderSideSize = bounds.height - sliderLabelInset * 2
         sliderView.frame = CGRect(x: fabs(CGFloat(selectedIndex) * (bounds.width - sliderSideSize) - sliderLabelInset), y: sliderLabelInset, width:  sliderSideSize, height: sliderSideSize)
--- SliderDemo/ViewController.swift
+++ SliderDemo/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var slider: SliderControl!
+    @IBOutlet weak var slider3: SliderControl!
+    @IBOutlet weak var slider4: SliderControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,22 @@ class ViewController: UIViewController {
         slider.leftSideTitle = "requesting…"
         slider.rightSideTitle = "slide to start"
         slider.sliderTitle = "START"
+
+        let slider2 = SliderControl(sliderTitle: "Check-in", leftSideTitle: "check-in…", rightSideTitle: "slide to check-in")
+        slider2.frame = CGRect(x: 50.0, y: 20.0, width: view.bounds.width - 100.0, height: 100.0)
+        slider2.autoresizingMask = [.flexibleWidth]
+        view.addSubview(slider2)
+
+        slider3.rightSideTitle = "slide to unlock"
+        slider3.sliderTitle = "START"
+        slider3.leftSideTitle = "slide to lock"
+        slider3.isEnabled = false
+
+        slider4.selectedIndex = 1
+        slider4.rightSideTitle = "slide to unlock"
+        slider4.sliderTitle = "STOP"
+        slider4.leftSideTitle = "slide to lock"
+        slider4.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,15 +45,43 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sliderValueChanged(_ sender: SliderControl) {
-        sender.showActivityIndicator()
-        slider.sliderTitle = sender.selectedIndex == 0 ? "START" : "STOP"
+//        if sender.controlState != .error {
+//            sender.showActivityIndicator()
+//            sender.sliderTitle = sender.selectedIndex == 0 ? "START" : "STOP"
+//            sender.isEnabled = false
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//                sender.isEnabled = true
+//                sender.hideActivityIndicator()
+//
+//                let isError = Bool(truncating: arc4random() % 2 as NSNumber);
+//
+//                if isError {
+//                    sender.leftSideTitle = sender.selectedIndex == 0 ? "requesting…" : "error"
+//                    sender.rightSideTitle = sender.selectedIndex == 0 ? "error" : "slide to start"
+//                    sender.controlState = .error
+//                    sender.setSelectedIndex(1 - sender.selectedIndex, animated: true, animationDelay: 2)
+//                } else {
+//                    sender.rightSideTitle = sender.selectedIndex == 0 ? "slide to start" : "requesting…"
+//                    sender.leftSideTitle = sender.selectedIndex == 0 ? "requesting…" : "done"
+//                }
+//            }
+//        } else {
+//            sender.controlState = .normal
+//            sender.leftSideTitle = sender.selectedIndex == 0 ? "requesting…" : "done"
+//            sender.rightSideTitle = sender.selectedIndex == 0 ? "slide to start" : "done"
+//        }
+//    }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            sender.hideActivityIndicator()
-            sender.rightSideTitle = sender.selectedIndex == 0 ? "slide to start" : "requesting…"
-            sender.leftSideTitle = sender.selectedIndex == 0 ? "requesting…" : "done"
-        }
+            sender.showActivityIndicator()
+            sender.sliderTitle = sender.selectedIndex == 0 ? "START" : "STOP"
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                sender.hideActivityIndicator()
+
+                sender.rightSideTitle = sender.selectedIndex == 0 ? "slide to start" : "requesting…"
+                sender.leftSideTitle = sender.selectedIndex == 0 ? "requesting…" : "done"
+            }
     }
-
 }
 
